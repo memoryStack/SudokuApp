4af358f30d804952d1357deb96ed4dde98138e6d
Anuj Rao <anujabo186@gmail.com>
2021-11-21 15:34:32 +0530

    handle deeplink launch scenarios ad add fallback game launch for invalid scenarios

--- src/apps/arena/hooks/gameHandler.js
+++ src/apps/arena/hooks/gameHandler.js
@@ -14,6 +14,11 @@ import { RNSudokuPuzzle } from 'fast-sudoku-puzzles'
 import { usePrevious } from '../../../utils/customHooks'
 import { getKey } from '../../../utils/storage'
 import { cacheGameData, GAME_DATA_KEYS, PREVIOUS_GAME_DATA_KEY } from '../utils/cacheGameHandler'
+import {
+    INVALID_DEEPLINK_PUZZLE,
+    LAUNCHING_PREVIOUS_PUZZLE,
+    DEEPLINK_PUZZLE_NO_SOLUTIONS,
+} from '../../../resources/stringLiterals'
 
 const transformNativeGeneratedPuzzle = (clues, solution) => {
     const mainNumbers = new Array(9)
@@ -34,45 +39,52 @@ const transformNativeGeneratedPuzzle = (clues, solution) => {
     return mainNumbers
 }
 
-const transformDeeplinkPuzzle = url => {
-    console.log('deeplink url is', url)
+// returns true if the deeplink puzzle is valid
+const verifyDeeplinkAndStartPuzzle = url => {
     const startIndex = url.indexOf(DEEPLINK_HOST_NAME)
     if (startIndex === -1) {
-        // show popup about the format of the url
+        emit(EVENTS.SHOW_SNACK_BAR, { msg: `${INVALID_DEEPLINK_PUZZLE} ${LAUNCHING_PREVIOUS_PUZZLE}` })
+        return
     }
 
     const boardMainNumbers = url.substring(DEEPLINK_HOST_NAME.length)
-    console.log(boardMainNumbers)
-    if (boardMainNumbers.length < 81) {
-        // show some error  about the invalidity of the puzzle link
-    } else {
-        const mainNumbers = new Array(9)
-        let cellNo = 0
-        for (let row = 0; row < 9; row++) {
-            const rowData = new Array(9)
-            for (let col = 0; col < 9; col++) {
-                const clueIntValue = parseInt(boardMainNumbers[cellNo], 10)
-                if (isNaN(clueIntValue)) {
-                    // abort and show invalidity message
-                }
-                // const cellvalue = clues[cellNo]
-                rowData[col] = {
-                    value: clueIntValue,
-                    solutionValue: '',
-                    isClue: clueIntValue !== 0,
-                }
-                cellNo++
-            }
-            mainNumbers[row] = rowData
-        }
+    if (boardMainNumbers.length !== 81) {
+        emit(EVENTS.SHOW_SNACK_BAR, { msg: `${INVALID_DEEPLINK_PUZZLE} ${LAUNCHING_PREVIOUS_PUZZLE}` })
+        return
+    }
 
-        // let's check if the puzzle is valid or not ?
-        if (getNumberOfSolutions(mainNumbers) === 1) {
-            // puzzle can be started
-            emit(EVENTS.START_DEEPLINK_PUZZLE, { mainNumbers })
-        } else {
-            // show  error message and  don't start the puzzle
+    const mainNumbers = new Array(9)
+    let cellNo = 0
+    for (let row = 0; row < 9; row++) {
+        const rowData = new Array(9)
+        for (let col = 0; col < 9; col++) {
+            const clueIntValue = parseInt(boardMainNumbers[cellNo], 10)
+            if (isNaN(clueIntValue)) {
+                emit(EVENTS.SHOW_SNACK_BAR, { msg: `${INVALID_DEEPLINK_PUZZLE} ${LAUNCHING_PREVIOUS_PUZZLE}` })
+                return
+            }
+            rowData[col] = {
+                value: clueIntValue,
+                solutionValue: '',
+                isClue: clueIntValue !== 0,
+            }
+            cellNo++
         }
+        mainNumbers[row] = rowData
+    }
+
+    // TODO: invalid puzzles scenarios can be handled in a better way, the ways that
+    // will give better insight to the user about what's went wrong
+    const numberOfSolutions = getNumberOfSolutions(mainNumbers)
+    switch (numberOfSolutions) {
+        case 0:
+            emit(EVENTS.SHOW_SNACK_BAR, { msg: `${DEEPLINK_PUZZLE_NO_SOLUTIONS} ${LAUNCHING_PREVIOUS_PUZZLE}` })
+            break
+        case 1:
+            emit(EVENTS.START_DEEPLINK_PUZZLE, { mainNumbers })
+            return true
+        default:
+            emit(EVENTS.SHOW_SNACK_BAR, { msg: `${DEEPLINK_PUZZLE_NO_SOLUTIONS} ${LAUNCHING_PREVIOUS_PUZZLE}` })
     }
 }
 
@@ -81,9 +93,38 @@ const useManageGame = () => {
     const previousGameState = usePrevious(gameState)
     const [showNextGameMenu, setShowNextGameMenu] = useState(false)
 
-    // resume previous game or start new game of previously solved level or take care of deeplinking
+    const handleDeeplinkPuzzleLaunch = url => {
+        let startDefaultPuzzleProcess = true
+        if (url) startDefaultPuzzleProcess = !verifyDeeplinkAndStartPuzzle(url)
+        startDefaultPuzzleProcess && emit(EVENTS.START_DEFAULT_GAME_PROCESS)
+    }
+
     useEffect(() => {
-        const startPreviousGame = async () => {
+        Linking.getInitialURL()
+            .then(url => {
+                handleDeeplinkPuzzleLaunch(url)
+            })
+            .catch(error => {
+                console.log(__DEV__ && error)
+                emit(EVENTS.START_DEFAULT_GAME_PROCESS)
+            })
+    }, [])
+
+    // app was in background and brought to fore-ground by deeplink
+    useEffect(() => {
+        const handler = ({ url }) => handleDeeplinkPuzzleLaunch(url)
+        Linking.addEventListener('url', handler)
+        return () => Linking.removeEventListener('url', handler)
+    }, [])
+
+    useEffect(() => {
+        const handler = newState => newState && setGameState(newState)
+        addListener(EVENTS.CHANGE_GAME_STATE, handler)
+        return () => removeListener(EVENTS.CHANGE_GAME_STATE, handler)
+    }, [])
+
+    useEffect(() => {
+        const handler = async () => {
             const previousGameData = await getKey(PREVIOUS_GAME_DATA_KEY)
             if (previousGameData) {
                 const state = previousGameData[GAME_DATA_KEYS.STATE]
@@ -104,22 +145,8 @@ const useManageGame = () => {
                 emit(EVENTS.GENERATE_NEW_PUZZLE, { difficultyLevel: LEVEL_DIFFICULTIES.EASY })
             }
         }
-
-        Linking.getInitialURL()
-            .then(url => {
-                if (url) transformDeeplinkPuzzle(url)
-                else startPreviousGame()
-            })
-            .catch(error => {
-                console.log(__DEV__ && error)
-                startPreviousGame()
-            })
-    }, [])
-
-    useEffect(() => {
-        const handler = newState => newState && setGameState(newState)
-        addListener(EVENTS.CHANGE_GAME_STATE, handler)
-        return () => removeListener(EVENTS.CHANGE_GAME_STATE, handler)
+        addListener(EVENTS.START_DEFAULT_GAME_PROCESS, handler)
+        return () => removeListener(EVENTS.START_DEFAULT_GAME_PROCESS, handler)
     }, [])
 
     useEffect(() => {
@@ -127,7 +154,6 @@ const useManageGame = () => {
         const handler = ({ difficultyLevel }) => {
             if (!difficultyLevel) return
             // "minClues" becoz sometimes for the expert type of levels we get more than desired clues
-            const time = Date.now()
             const minClues = LEVELS_CLUES_INFO[difficultyLevel]
             // now as i changed the position of "timeTaken" reading after the puzzle has
             // been generated looks like that puzzle algo was never a big issue. it was just the setStates latency
@@ -135,8 +161,6 @@ const useManageGame = () => {
             RNSudokuPuzzle.getSudokuPuzzle(minClues)
                 .then(({ clues, solution }) => {
                     if (!componentUnmounted) {
-                        timeTaken = Date.now() - time
-                        console.log('@@@@@@@@ time taken is to generate new puzzle is', timeTaken)
                         const mainNumbers = transformNativeGeneratedPuzzle(clues, solution)
                         emit(EVENTS.START_NEW_GAME, { difficultyLevel, mainNumbers })
                         setGameState(GAME_STATE.ACTIVE)
@@ -167,6 +191,7 @@ const useManageGame = () => {
         }
     }, [transformNativeGeneratedPuzzle])
 
+    // these 2 below listeners can be combined
     useEffect(() => {
         const handler = ({ mainNumbers }) => {
             // drag the customPuzzle HC and we can simply unmount the
@@ -187,13 +212,6 @@ const useManageGame = () => {
         addListener(EVENTS.START_DEEPLINK_PUZZLE, handler)
         return () => removeListener(EVENTS.START_DEEPLINK_PUZZLE, handler)
     }, [])
-
-    // app was in background ad brought to fore-ground by deeplink
-    useEffect(() => {
-        const handler = ({ url }) => transformDeeplinkPuzzle(url)
-        Linking.addEventListener('url', handler)
-        return () => Linking.removeEventListener('url', handler)
-    }, [transformDeeplinkPuzzle])
 
     // cache game data
     useEffect(() => {
--- src/resources/stringLiterals.js
+++ src/resources/stringLiterals.js
@@ -1,4 +1,8 @@
 export const NEW_GAME = 'New Game',
     SHARE = 'Share',
     PLAY = 'Play',
-    SOMETHING_WENT_WRONG = 'Something went wrong. Please try again.'
+    SOMETHING_WENT_WRONG = 'Something went wrong. Please try again.',
+    INVALID_DEEPLINK_PUZZLE = 'Invalid Url.',
+    LAUNCHING_PREVIOUS_PUZZLE = 'Launching previous puzzle',
+    DEEPLINK_PUZZLE_NO_SOLUTIONS = 'Puzzle has no solutions.',
+    DEEPLINK_PUZZLE_MULTIPLE_SOLUTIONS = 'Puzzle has multiple solutions.'
