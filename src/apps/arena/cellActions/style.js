58c510088626cf2f7f7216554ff8c69bf3349ea9
Anuj Rao <anujabo186@gmail.com>
2021-11-22 13:58:41 +0530

    use fontfamily for fastpencil text

--- src/apps/arena/cellActions/style.js
+++ src/apps/arena/cellActions/style.js
@@ -1,4 +1,5 @@
 import { StyleSheet } from 'react-native'
+import { fonts } from '../../../resources/fonts/font'
 
 const ACTION_CONTAINER_WIDTH = 60 // there are only 4 type of actions present
 export const INACTIVE_ICON_FILL = 'rgb(127, 127, 127)'
@@ -11,6 +12,7 @@ export const Styles = StyleSheet.create({
     actionText: {
         fontSize: 16,
         marginTop: 8,
+        fontFamily: fonts.regular,
         color: INACTIVE_ICON_FILL, // to match the icon color and below text color
     },
     hintsTickerBox: {
@@ -28,5 +30,6 @@ export const Styles = StyleSheet.create({
     hintsTickerText: {
         fontSize: 12,
         color: 'white',
+        fontFamily: fonts.regular,
     },
 })
