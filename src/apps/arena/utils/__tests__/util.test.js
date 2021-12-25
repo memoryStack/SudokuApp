638726e7d64a524b11f38aac1c9604a6de8e0eff
Anuj Rao <anujabo186@gmail.com>
2021-12-25 17:39:20 +0530

    ran prettier and added test cases for isGameOver function

--- src/apps/arena/utils/__tests__/util.test.js
+++ src/apps/arena/utils/__tests__/util.test.js
@@ -1,13 +1,34 @@
-import { getTimeComponentString } from '../util'
+import { getTimeComponentString, isGameOver } from '../util'
+import { GAME_STATE } from '../../../../resources/constants'
 
-test('getTimeComponentString test 1', () => {
-    expect(getTimeComponentString(0)).toBe('00')
+describe('time component value formatter', () => {
+    test('getTimeComponentString test 1', () => {
+        expect(getTimeComponentString(0)).toBe('00')
+    })
+
+    test('getTimeComponentString test 2', () => {
+        expect(getTimeComponentString(11)).toBe('11')
+    })
+
+    test('getTimeComponentString test 3', () => {
+        expect(getTimeComponentString(1)).toBe('01')
+    })
 })
 
-test('getTimeComponentString test 2', () => {
-    expect(getTimeComponentString(11)).toBe('11')
-})
+describe('is game over', () => {
+    test('isGameOver test 1', () => {
+        expect(isGameOver(GAME_STATE.ACTIVE)).toBe(false)
+    })
 
-test('getTimeComponentString test 3', () => {
-    expect(getTimeComponentString(1)).toBe('01')
+    test('isGameOver test 2', () => {
+        expect(isGameOver(GAME_STATE.INACTIVE)).toBe(false)
+    })
+
+    test('isGameOver test 3', () => {
+        expect(isGameOver(GAME_STATE.OVER_SOLVED)).toBe(true)
+    })
+
+    test('isGameOver test 4', () => {
+        expect(isGameOver(GAME_STATE.OVER_UNSOLVED)).toBe(true)
+    })
 })
--- src/apps/home/home.test.js
+++ src/apps/home/home.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 import renderer from 'react-test-renderer'
 import { Home } from '.'
 
 jest.useFakeTimers()
 test('renders correctly', () => {
-  const homeTree = renderer.create(<Home />).toJSON()
-  expect(homeTree).toMatchSnapshot()
+    const homeTree = renderer.create(<Home />).toJSON()
+    expect(homeTree).toMatchSnapshot()
 })
