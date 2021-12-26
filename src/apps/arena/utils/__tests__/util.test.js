a3c0444d00a4f9700dc510580239ff0a072a1998
Anuj Rao <anujabo186@gmail.com>
2021-12-26 07:28:34 +0530

    add test cases for shouldSaveGameState func

--- src/apps/arena/utils/__tests__/util.test.js
+++ src/apps/arena/utils/__tests__/util.test.js
@@ -1,4 +1,4 @@
-import { getTimeComponentString, isGameOver } from '../util'
+import { getTimeComponentString, isGameOver, shouldSaveGameState } from '../util'
 import { GAME_STATE } from '../../../../resources/constants'
 
 describe('time component value formatter', () => {
@@ -30,5 +30,24 @@ describe('is game over', () => {
 
     test('isGameOver test 4', () => {
         expect(isGameOver(GAME_STATE.OVER_UNSOLVED)).toBe(true)
+    })
+})
+
+describe('should cache game data', () => {
+    // if game is over then it will always save the state
+    test('shouldSaveGameState test 1', () => {
+        expect(shouldSaveGameState(GAME_STATE.OVER_SOLVED)).toBe(true)
+    })
+    
+    test('shouldSaveGameState test 2', () => {
+        expect(shouldSaveGameState(GAME_STATE.OVER_UNSOLVED)).toBe(true)
+    })
+
+    test('shouldSaveGameState test 3', () => {
+        expect(shouldSaveGameState(GAME_STATE.ACTIVE, GAME_STATE.INACTIVE)).toBe(false)
+    })
+
+    test('shouldSaveGameState test 4', () => {
+        expect(shouldSaveGameState(GAME_STATE.INACTIVE, GAME_STATE.ACTIVE)).toBe(true)
     })
 })
