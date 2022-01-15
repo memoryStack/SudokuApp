8011ff094ef3272f33f65f8ccd4c99ea9d7afafb
Anuj <anujabo186@gmail.com>
2022-01-15 13:27:14 +0530

    group function parameters, refactoring

--- src/apps/arena/utils/util.js
+++ src/apps/arena/utils/util.js
@@ -40,7 +40,8 @@ export const duplicacyPresent = (num, mainNumbers, cell) => {
     return 0
 }
 
-const checkDuplicateSolutions = (row, col, mainNumbers) => {
+const checkDuplicateSolutions = (mainNumbers, cell) => {
+    const { row, col } = cell
     if (row === 9) {
         if (++numOfSolutions > 1) return
         for (let row = 0; row < 9; row++) {
@@ -51,14 +52,14 @@ const checkDuplicateSolutions = (row, col, mainNumbers) => {
         }
         return
     }
-    if (col === 9) return checkDuplicateSolutions(row + 1, 0, mainNumbers)
-    if (mainNumbers[row][col].value) return checkDuplicateSolutions(row, col + 1, mainNumbers)
+    if (col === 9) return checkDuplicateSolutions(mainNumbers, { row: row + 1, col: 0 })
+    if (mainNumbers[row][col].value) return checkDuplicateSolutions(mainNumbers, { row, col: col + 1 })
 
     for (let num = 1; num <= 9; num++) {
         if (numOfSolutions > 1) break
         if (!duplicacyPresent(num, mainNumbers, { row, col })) {
             mainNumbers[row][col].value = num
-            checkDuplicateSolutions(row, col + 1, mainNumbers)
+            checkDuplicateSolutions(mainNumbers, { row, col: col + 1 })
             mainNumbers[row][col].value = 0
         }
     }
@@ -67,7 +68,7 @@ const checkDuplicateSolutions = (row, col, mainNumbers) => {
 // TODO: it's name should be changed. it is not returning number of solutions exactly
 export const getNumberOfSolutions = mainNumbers => {
     numOfSolutions = 0
-    checkDuplicateSolutions(0, 0, mainNumbers)
+    checkDuplicateSolutions(mainNumbers, { row: 0, col: 0 })
     if (numOfSolutions === 1) {
         for (let row = 0; row < 9; row++) {
             for (let col = 0; col < 9; col++) {
