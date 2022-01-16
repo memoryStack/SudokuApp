20a7f7dabc56590a7b442ee92ced07a408b6fabd
Anuj <anujabo186@gmail.com>
2022-01-16 12:09:25 +0530

    seperate nakedGroup smart hint logic

--- src/apps/arena/utils/smartHints/constants.js
+++ src/apps/arena/utils/smartHints/constants.js
@@ -1,3 +1,5 @@
+import { Styles as boardStyles } from '../../gameBoard/style'
+
 const NAKED_SINGLE_TYPES = {
     ROW: 'row',
     COL: 'col',
@@ -11,4 +13,13 @@ const HIDDEN_SINGLE_TYPES = {
     BLOCK: 'block',
 }
 
-export { NAKED_SINGLE_TYPES, HIDDEN_SINGLE_TYPES }
+/*
+    TODO: come upp with better colors later on
+    let's focus on the functionality for now
+*/
+const SMART_HINTS_CELLS_BG_COLOR = {
+    SELECTED: boardStyles.selectedCellBGColor,
+    IN_FOCUS_DEFAULT: boardStyles.defaultCellBGColor,
+}
+
+export { NAKED_SINGLE_TYPES, HIDDEN_SINGLE_TYPES, SMART_HINTS_CELLS_BG_COLOR }
