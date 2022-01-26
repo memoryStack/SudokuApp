99f000f04cf854c9cb826cf2428452f5d04b0f6c
Anuj <anujabo186@gmail.com>
2022-01-27 00:17:21 +0530

    refactor nakedSingle function

--- src/apps/arena/utils/smartHints/nakedSingle/nakedSingle.js
+++ src/apps/arena/utils/smartHints/nakedSingle/nakedSingle.js
@@ -1,4 +1,5 @@
 import { getBlockAndBoxNum, getRowAndCol } from '../../../../../utils/util'
+import { isCellEmpty } from '../../util'
 import { NAKED_SINGLE_TYPES } from '../constants'
 
 const isNakedSinglePresent = cellNotes => {
@@ -19,39 +20,48 @@ const isNakedSinglePresent = cellNotes => {
     }
 }
 
+const isOnlyEmptyCellInRow = (cell, mainNumbers) => {
+    let cellsFilled = 0
+    for (let col = 0; col < 9; col++) {
+        if (!isCellEmpty({ row: cell.row, col }, mainNumbers)) cellsFilled++
+    }
+    return cellsFilled === 8
+}
+
+const isOnlyEmptyCellInCol = (cell, mainNumbers) => {
+    let cellsFilled = 0
+    for (let row = 0; row < 9; row++) {
+        if (!isCellEmpty({ row, col: cell.col }, mainNumbers)) cellsFilled++
+    }
+    return cellsFilled === 8
+}
+
+const isOnlyEmptyCellInBlock = (cell, mainNumbers) => {
+    let cellsFilled = 0
+    const { blockNum } = getBlockAndBoxNum(cell)
+    for (let boxNum = 0; boxNum < 9; boxNum++) {
+        if (!isCellEmpty(getRowAndCol(blockNum, boxNum), mainNumbers)) cellsFilled++
+    }
+    return cellsFilled === 8
+}
+
+const getHouseType = (cell, mainNumbers) => {
+    if (isOnlyEmptyCellInRow(cell, mainNumbers)) return NAKED_SINGLE_TYPES.ROW
+    if (isOnlyEmptyCellInCol(cell, mainNumbers)) return NAKED_SINGLE_TYPES.COL
+    if (isOnlyEmptyCellInBlock(cell, mainNumbers)) return NAKED_SINGLE_TYPES.BLOCK
+    return NAKED_SINGLE_TYPES.MIX
+}
+
 export const getAllNakedSingles = (mainNumbers, notesInfo) => {
     const nakedSingles = []
     for (let row = 0; row < 9; row++) {
         for (let col = 0; col < 9; col++) {
             if (mainNumbers[row][col].value) continue
             // TODO: change "mainNumber" field name. it doesn't feel right.
-            const { present: nakedSinglePresent, mainNumber } = isNakedSinglePresent(notesInfo[row][col])
-            if (nakedSinglePresent)
-                nakedSingles.push({ cell: { row, col }, mainNumber, type: getSingleType({ row, col }, mainNumbers) })
+            const { present, mainNumber } = isNakedSinglePresent(notesInfo[row][col])
+            if (present)
+                nakedSingles.push({ cell: { row, col }, mainNumber, type: getHouseType({ row, col }, mainNumbers) })
         }
     }
     return nakedSingles
-}
-
-const getSingleType = ({ row, col }, mainNumbers) => {
-    let candidatesFilled = 0
-    for (let col = 0; col < 9; col++) {
-        if (mainNumbers[row][col].value) candidatesFilled++
-    }
-    if (candidatesFilled === 8) return NAKED_SINGLE_TYPES.ROW
-
-    candidatesFilled = 0
-    for (let row = 0; row < 9; row++) {
-        if (mainNumbers[row][col].value) candidatesFilled++
-    }
-    if (candidatesFilled === 8) return NAKED_SINGLE_TYPES.COL
-
-    candidatesFilled = 0
-    const { blockNum } = getBlockAndBoxNum({ row, col })
-    for (let boxNum = 0; boxNum < 9; boxNum++) {
-        const { row, col } = getRowAndCol(blockNum, boxNum)
-        if (mainNumbers[row][col].value) candidatesFilled++
-    }
-    if (candidatesFilled === 8) return NAKED_SINGLE_TYPES.BLOCK
-    return NAKED_SINGLE_TYPES.MIX
 }
