a772f56b8e4f8c43654ae31b00bae5cd58e0c885
Anuj Rao <anujabo186@gmail.com>
2022-05-22 08:50:44 +0530

    refactor naked group validity checker

--- src/apps/arena/utils/smartHints/validityTest/nakedGroup.js
+++ src/apps/arena/utils/smartHints/validityTest/nakedGroup.js
@@ -1,12 +1,8 @@
 export const isValidNakedGroup = ({ groupCandidates, hostCells }, _, possibleNotes) => {
     const extraCandidatePossible = hostCells.some(({ row, col }) => {
-        const cellNotes = possibleNotes[row][col]
-        return cellNotes.some(({ show, noteValue }) => {
-            if (!show) return false
-            // can use includes here
-            return groupCandidates.indexOf(noteValue) === -1
+        return possibleNotes[row][col].some(({ show, noteValue }) => {
+            return show && !groupCandidates.includes(noteValue)
         })
     })
-
     return !extraCandidatePossible
 }
