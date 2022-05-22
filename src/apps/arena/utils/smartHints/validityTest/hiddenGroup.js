8f1e9fb30b7f0d98def9cacb1cde018e295812b5
Anuj Rao <anujabo186@gmail.com>
2022-05-22 12:17:24 +0530

    refactor hidden groups validity checker

--- src/apps/arena/utils/smartHints/validityTest/hiddenGroup.js
+++ src/apps/arena/utils/smartHints/validityTest/hiddenGroup.js
@@ -1,17 +1,12 @@
-import { getHouseCells } from '../../houseCells'
-import { getCellHouseInfo, isCellExists } from '../../util'
+import { filterHouseCells, getCellHouseInfo, isCellExists } from '../../util'
 
 export const isValidHiddenGroup = ({ houseType, groupCandidates, hostCells }, _, possibleNotes) => {
     const { num: houseNum } = getCellHouseInfo(houseType, hostCells[0])
-    const houseCells = getHouseCells(houseType, houseNum)
-    const cellsWithoutHostCells = houseCells.filter(cell => {
+    const isAnyNotePresentInOtherCell = filterHouseCells({ type: houseType, num: houseNum }, (cell) => {
         return !isCellExists(cell, hostCells)
-    })
-
-    const isAnyNotePresentInOtherCell = cellsWithoutHostCells.some(cell => {
-        const cellNotes = possibleNotes[cell.row][cell.col]
-        return cellNotes.some(({ show, noteValue }) => {
-            return show && groupCandidates.indexOf(noteValue) !== -1
+    }).some(cell => {
+        return possibleNotes[cell.row][cell.col].some(({ show, noteValue }) => {
+            return show && groupCandidates.includes(noteValue)
         })
     })
 
