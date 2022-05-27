c9cdad2091a9080e10937ad406eb968de238d2db
Anuj Rao <anujabo186@gmail.com>
2022-05-27 20:04:43 +0530

    add support for onAction prop alias

--- src/utils/hocs/withActions.js
+++ src/utils/hocs/withActions.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react'
 
+const DEFAULT_ON_ACTION_PROP_NAME = 'onAction'
 const DEFAULT_OPTIONS = {
     shouldForwardAction: false,
 }
 
-const withActions = ({ actionHandlers = {}, initialState = {}, options = DEFAULT_OPTIONS }) =>
+const withActions = ({ actionHandlers = {}, onActionPropAlias = DEFAULT_ON_ACTION_PROP_NAME, initialState = {}, options = DEFAULT_OPTIONS }) =>
     function wrapComponent(WrappedComponent) {
         class WithActions extends PureComponent {
             state = initialState
@@ -29,11 +30,25 @@ const withActions = ({ actionHandlers = {}, initialState = {}, options = DEFAULT
                     result = onAction(action, result)
                 }
 
+                const test = 'zzz'
+
                 return result
             }
 
+            getOnActionProp = () => {
+                return {
+                    [onActionPropAlias]: this.handleAction
+                }
+            }
+
             render() {
-                return <WrappedComponent {...this.props} {...this.state} onAction={this.handleAction} />
+                return (
+                    <WrappedComponent
+                        {...this.props}
+                        {...this.state}
+                        {...this.getOnActionProp()}
+                    />
+                )
             }
         }
 
