60313f3ee33507717f339b64d8ae477a9de09578
Anuj Rao <anujabo186@gmail.com>
2022-05-28 07:42:09 +0530

    add action handlers for cell click and input panel click

--- src/apps/arena/smartHintHC/actionHandlers.js
+++ src/apps/arena/smartHintHC/actionHandlers.js
@@ -2,6 +2,8 @@ import { GAME_STATE } from '../../../resources/constants'
 import { updateSelectedCell } from '../store/actions/board.actions'
 import { updateGameState } from '../store/actions/gameState.actions'
 import { clearHints, showNextHint, showPrevHint, resetStoreState } from '../store/actions/smartHintHC.actions'
+import { ACTION_TYPES as INPUT_PANEL_ACTION_TYPES } from '../inputPanel/constants'
+import { ACTION_TYPES as BOARD_GENERIC_ACTION_TYPES } from '../gameBoard/actionTypes'
 
 const handleOnClose = ({ params: newCellToSelect }) => {
     if (newCellToSelect) updateSelectedCell(newCellToSelect)
@@ -19,11 +21,25 @@ const handleResetStoreState = () => {
     resetStoreState()
 }
 
+const handleCellClick = () => {
+
+}
+
+const handleNumberClick = () => {
+
+}
+
+const handleEraserClick = () => {
+
+}
+
 const ACTION_TYPES = {
     ON_CLOSE: 'ON_CLOSE',
     ON_NEXT_CLICK: 'ON_NEXT_CLICK',
     ON_PREV_CLICK: 'ON_PREV_CLICK',
     ON_UNMOUNT: 'ON_UNMOUNT',
+    ...INPUT_PANEL_ACTION_TYPES,
+    ...BOARD_GENERIC_ACTION_TYPES,
 }
 
 const ACTION_HANDLERS = {
@@ -31,6 +47,16 @@ const ACTION_HANDLERS = {
     [ACTION_TYPES.ON_NEXT_CLICK]: handleNextClick,
     [ACTION_TYPES.ON_PREV_CLICK]: handlePrevClick,
     [ACTION_TYPES.ON_UNMOUNT]: handleResetStoreState,
+    [BOARD_GENERIC_ACTION_TYPES.ON_CELL_PRESS]: handleCellClick,
+    [INPUT_PANEL_ACTION_TYPES.ON_ERASE_CLICK]: handleEraserClick,
+    [INPUT_PANEL_ACTION_TYPES.ON_NUMBER_CLICK]: handleNumberClick,
 }
 
 export { ACTION_TYPES, ACTION_HANDLERS }
+
+/*
+    try-out store
+        make a sub-tree in smartHintHC store only
+    try-out actionHandler 
+        keep it in this file only
+*/
