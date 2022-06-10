ab4a39c5baef1a85c422607c704e03d12999d07e
Anuj Rao <anujabo186@gmail.com>
2022-06-10 07:55:25 +0530

    use candidatesList generator for naked double try out analyser

--- src/apps/arena/utils/smartHints/tryOutInputAnalyser/nakedDouble.js
+++ src/apps/arena/utils/smartHints/tryOutInputAnalyser/nakedDouble.js
@@ -1,8 +1,9 @@
 import { getTryOutMainNumbers } from '../../../store/selectors/smartHintHC.selectors'
 import { getStoreState } from '../../../../../redux/dispatch.helpers'
-import { isCellEmpty } from '../../util'
 import { TRY_OUT_RESULT_STATES } from './constants'
 import { noInputInTryOut, getTryOutErrorType, getNakedGroupNoTryOutInputResult, getTryOutErrorResult, getCorrectFilledTryOutCandidates, getCandidatesToBeFilled } from './helpers'
+import { getCandidatesListText } from '../util'
+import { HINT_TEXT_CANDIDATES_JOIN_CONJUGATION } from '../constants'
 
 const tryOutAnalyser = ({ groupCandidates, focusedCells, groupCells }) => {
 
@@ -15,37 +16,25 @@ const tryOutAnalyser = ({ groupCandidates, focusedCells, groupCells }) => {
         return getTryOutErrorResult(tryOutErrorType)
     }
 
-    // one or more candidates are filled in correct place. prepare messages for this state.
-    // TODO: replace it with the newly added helper func for this purpose
-    const getFilledCandidatesListForGreenState = candidates => {
-        if (candidates.length === 1) return `${candidates[0]}`
-
-        return candidates.reduce((prevValue, currentCandidate, currentIndex) => {
-            if (currentIndex === 0) return currentCandidate
-
-            const isLastElement = currentIndex === candidates.length - 1
-            const joint = isLastElement ? ' and ' : ', '
-            return prevValue + joint + currentCandidate
-        }, '')
-    }
-
     const tryOutMainNumbers = getTryOutMainNumbers(getStoreState())
     const correctlyFilledGroupCandidates = getCorrectFilledTryOutCandidates(groupCells, tryOutMainNumbers)
     if (correctlyFilledGroupCandidates.length === groupCandidates.length) {
+        const candidatesList = getCandidatesListText(correctlyFilledGroupCandidates, HINT_TEXT_CANDIDATES_JOIN_CONJUGATION.AND)
         return {
             msg:
-                `${getFilledCandidatesListForGreenState(correctlyFilledGroupCandidates)} are filled in` +
+                `${candidatesList} are filled in` +
                 ` these cells without any error. now we are sure` +
-                ` that ${getFilledCandidatesListForGreenState(correctlyFilledGroupCandidates)}` +
+                ` that ${candidatesList}` +
                 ` can't come in cells where these were highlighted in red`,
             state: TRY_OUT_RESULT_STATES.VALID_PROGRESS,
         }
     } else {
         const candidatesToBeFilled = getCandidatesToBeFilled(correctlyFilledGroupCandidates, groupCandidates)
+        const candidatesList = getCandidatesListText(candidatesToBeFilled, HINT_TEXT_CANDIDATES_JOIN_CONJUGATION.AND)
         return {
             msg:
-                `fill ${getFilledCandidatesListForGreenState(candidatesToBeFilled)} as well` +
-                ` to find where these numbers can't come in the highlighted region.`,
+                `fill ${candidatesList} as well to find where`
+                + ` these numbers can't come in the highlighted region.`,
             state: TRY_OUT_RESULT_STATES.VALID_PROGRESS,
         }
     }
