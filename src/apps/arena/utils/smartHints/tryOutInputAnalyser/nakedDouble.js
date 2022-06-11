184d945139f72b4d5859817f3ea0758ad9ec9fc3
Anuj Rao <anujabo186@gmail.com>
2022-06-11 07:21:31 +0530

    extract funcs for preparing valid progress tryout result for naked double

--- src/apps/arena/utils/smartHints/tryOutInputAnalyser/nakedDouble.js
+++ src/apps/arena/utils/smartHints/tryOutInputAnalyser/nakedDouble.js
@@ -19,24 +19,30 @@ const tryOutAnalyser = ({ groupCandidates, focusedCells, groupCells }) => {
     const tryOutMainNumbers = getTryOutMainNumbers(getStoreState())
     const correctlyFilledGroupCandidates = getCorrectFilledTryOutCandidates(groupCells, tryOutMainNumbers)
     if (correctlyFilledGroupCandidates.length === groupCandidates.length) {
-        const candidatesList = getCandidatesListText(correctlyFilledGroupCandidates, HINT_TEXT_CANDIDATES_JOIN_CONJUGATION.AND)
-        return {
-            msg:
-                `${candidatesList} are filled in` +
-                ` these cells without any error. now we are sure` +
-                ` that ${candidatesList}` +
-                ` can't come in cells where these were highlighted in red`,
-            state: TRY_OUT_RESULT_STATES.VALID_PROGRESS,
-        }
+        return getAllInputsFilledResult(groupCandidates)
     } else {
         const candidatesToBeFilled = getCandidatesToBeFilled(correctlyFilledGroupCandidates, groupCandidates)
-        const candidatesList = getCandidatesListText(candidatesToBeFilled, HINT_TEXT_CANDIDATES_JOIN_CONJUGATION.AND)
-        return {
-            msg:
-                `fill ${candidatesList} as well to find where`
-                + ` these numbers can't come in the highlighted region.`,
-            state: TRY_OUT_RESULT_STATES.VALID_PROGRESS,
-        }
+        return getPartialCorrectlyFilledResult(candidatesToBeFilled)
+    }
+}
+
+const getAllInputsFilledResult = (groupCandidates) => {
+    const candidatesList = getCandidatesListText(groupCandidates, HINT_TEXT_CANDIDATES_JOIN_CONJUGATION.AND)
+    return {
+        msg: `${candidatesList} are filled in` +
+            ` these cells without any error. now we are sure` +
+            ` that ${candidatesList}` +
+            ` can't come in cells where these were highlighted in red`,
+        state: TRY_OUT_RESULT_STATES.VALID_PROGRESS,
+    }
+}
+
+const getPartialCorrectlyFilledResult = (candidatesToBeFilled) => {
+    const candidatesList = getCandidatesListText(candidatesToBeFilled, HINT_TEXT_CANDIDATES_JOIN_CONJUGATION.AND)
+    return {
+        msg: `fill ${candidatesList} as well to find where`
+            + ` these numbers can't come in the highlighted region.`,
+        state: TRY_OUT_RESULT_STATES.VALID_PROGRESS,
     }
 }
 
