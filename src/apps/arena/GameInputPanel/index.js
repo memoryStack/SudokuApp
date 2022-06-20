6a662ed652224f5d5b714cc4f2b2804905ef1817
Anuj Rao <anujabo186@gmail.com>
2022-06-21 00:57:41 +0530

    use board looper for game input numbers visibility calculator

--- src/apps/arena/GameInputPanel/index.js
+++ src/apps/arena/GameInputPanel/index.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import withActions from '../../../utils/hocs/withActions'
 import { Inputpanel } from '../inputPanel'
 import { getMainNumbers } from '../store/selectors/board.selectors'
+import { forBoardEachCell } from '../utils/util'
 import { ACTION_HANDLERS } from './actionHandlers'
 
 const GameInputPanel_ = ({ onAction }) => {
@@ -11,15 +12,12 @@ const GameInputPanel_ = ({ onAction }) => {
 
     useEffect(() => {
         const instancesCountAfterUpdate = new Array(10).fill(0)
-        // BOARD_LOOPER: 2
-        for (let row = 0; row < 9; row++) {
-            for (let col = 0; col < 9; col++) {
-                const value = mainNumbers[row][col].value
-                if (value === mainNumbers[row][col].solutionValue) {
-                    instancesCountAfterUpdate[value]++
-                }
+        forBoardEachCell(({ row, col }) => {
+            const value = mainNumbers[row][col].value
+            if (value === mainNumbers[row][col].solutionValue) {
+                instancesCountAfterUpdate[value]++
             }
-        }
+        })
 
         const numbersVisibilityStatusAfterUpdate = [false]
         for (let i = 1; i <= 9; i++) {
