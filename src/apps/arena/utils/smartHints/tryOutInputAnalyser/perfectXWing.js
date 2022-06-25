7bde58dc5ee9b4286ad98a687c354d794e901840
Anuj Rao <anujabo186@gmail.com>
2022-06-25 20:51:49 +0530

    find out all cases when removable note cell is filled

--- src/apps/arena/utils/smartHints/tryOutInputAnalyser/perfectXWing.js
+++ src/apps/arena/utils/smartHints/tryOutInputAnalyser/perfectXWing.js
@@ -1,14 +1,22 @@
-import { getHouseAxesValue } from "../../util"
+import { getHouseAxesValue, isCellEmpty } from "../../util"
 import { HOUSE_TYPE, HOUSE_TYPE_VS_FULL_NAMES } from "../constants"
 import { TRY_OUT_RESULT_STATES } from "./constants"
 import { noInputInTryOut } from "./helpers"
 import { toOrdinal } from "../../../../../utils/utilities/toOrdinal"
+import { getTryOutMainNumbers } from "../../../store/selectors/smartHintHC.selectors"
+import { getStoreState } from "../../../../../redux/dispatch.helpers"
 
 export default ({ xWing, xWingCells, removableNotesHostCells }) => {
 
     if (noInputInTryOut([...xWingCells, ...removableNotesHostCells])) {
         return getNoInputResult(xWing)
     }
+
+    if (filterFilledCells(removableNotesHostCells).length) {
+        return getRemovableNoteHostCellFilledResult(xWing, xWingCells, removableNotesHostCells)
+    }
+
+    //  todo: one or two correct cells are filled
 
     return {
         msg: 'karya pragati path per h',
@@ -47,5 +55,39 @@ const getXWingHousesTexts = (houseType, xWingLegs) => {
     return {
         houseAAxesValue,
         houseBAxesValue,
+    }
+}
+
+const filterFilledCells = (removableNotesHostCells) => {
+    const mainNumbers = getTryOutMainNumbers(getStoreState())
+    return removableNotesHostCells.filter((cell) => {
+        return !isCellEmpty(cell, mainNumbers)
+    })
+}
+
+const getRemovableNoteHostCellFilledResult = (xWing, xWingCells, removableNotesHostCells) => {
+    const removableNotesHostCellsFilledCount = filterFilledCells(removableNotesHostCells).length
+    if (removableNotesHostCellsFilledCount === 2) {
+        // both houses will be without the candidate
+        return {
+            msg: 'dono hi glt h re budhbuk',
+            state: TRY_OUT_RESULT_STATES.ERROR,
+        }
+    }
+
+    const xWingFilledCellsCount = filterFilledCells(xWingCells).length
+    if (xWingFilledCellsCount) {
+        // 1 wrong filled and 1 correctly filled
+        return {
+            msg: 'ek sahi or ek glt h',
+            state: TRY_OUT_RESULT_STATES.ERROR,
+        }
+    }
+
+    // only one wrongly cell is filled
+
+    return {
+        msg: '1 glti kiya ntkht',
+        state: TRY_OUT_RESULT_STATES.ERROR,
     }
 }
