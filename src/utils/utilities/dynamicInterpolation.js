0ee5206b55fd18369408d835661bf253f0e2e48f
Anuj Rao <anujabo186@gmail.com>
2022-07-17 18:18:50 +0530

    1. add support for non string values in dynamicInterpolation 2. add test cases for dynamic interpolation

--- src/utils/utilities/dynamicInterpolation.js
+++ src/utils/utilities/dynamicInterpolation.js
@@ -1,5 +1,7 @@
 const interpolationPattern = /{{(.*?)}}/g
 
 export const dynamicInterpolation = (template = '', values = {}, pattern = interpolationPattern) => {
+    if (typeof template !== 'string') return ''
+
     return template.replace(pattern, (match, key) => values[key] || match)
 }
