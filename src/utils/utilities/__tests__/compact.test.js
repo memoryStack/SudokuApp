33eea2968de141c251c1332ec206876449a68fcc
Anuj Rao <anujabo186@gmail.com>
2022-07-19 10:49:51 +0530

    add non-array input test case for compact lodash function

--- src/utils/utilities/__tests__/compact.test.js
+++ src/utils/utilities/__tests__/compact.test.js
@@ -5,8 +5,8 @@ describe('_compact()', () => {
     /* _compact(array)    */
 
     test('returns an array after all the flasey values removed like [null, undefined, empty string, 0, false, NaN]', () => {
-        const array = [0, 1, null, false, 2, '', 3, undefined, true, NaN]
-        const expectedResult = [1, 2, 3, true]
+        const array = [0, 1, null, false, 2, '', 3, undefined, true, NaN, {}]
+        const expectedResult = [1, 2, 3, true, {}]
         expect(_compact(array)).toStrictEqual(expectedResult)
     })
 
@@ -14,6 +14,14 @@ describe('_compact()', () => {
         const array = [1, 2, 3]
         expect(_compact(array)).not.toBe(array)
         expect(_compact(array)).toStrictEqual(array)
+    })
+
+    test('returns an empty array when non-empty array is passed', () => {
+        expect(_compact(null)).toStrictEqual([])
+        expect(_compact(undefined)).toStrictEqual([])
+        expect(_compact(0)).toStrictEqual([])
+        expect(_compact(false)).toStrictEqual([])
+        expect(_compact({})).toStrictEqual([])
     })
 
 })
