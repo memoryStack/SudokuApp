e9b0d99c3602a4b9a7f57597f1f36cb3eb8f3931
Anuj Rao <anujabo186@gmail.com>
2022-09-06 16:21:20 +0530

    add game state class

---
+++ src/apps/arena/utils/classes/__tests__/gameState.test.js
@@ -0,0 +1,52 @@
+import { GAME_STATE } from '../../../../../resources/constants'
+import { GameState } from '../gameState'
+
+describe('GameState class', () => {
+    test('creates a valid gameState object', () => {
+        expect(new GameState(GAME_STATE.ACTIVE)).toBeInstanceOf(GameState)
+    })
+
+    test('throws exception when initialized with invalid value', () => {
+        expect(() => new GameState('sleeping')).toThrow(Error)
+    })
+
+    test('isValidValue static method checks validity of passed value to initialize', () => {
+        expect(GameState.isValidValue('invalid')).toBeFalsy()
+        expect(GameState.isValidValue('')).toBeFalsy()
+        expect(GameState.isValidValue(null)).toBeFalsy()
+        expect(GameState.isValidValue(undefined)).toBeFalsy()
+        expect(GameState.isValidValue()).toBeFalsy()
+        expect(GameState.isValidValue(GAME_STATE.ACTIVE)).toBeTruthy()
+    })
+
+    test('getGameState getter returns state with which object was initialised', () => {
+        const gameState = new GameState(GAME_STATE.ACTIVE)
+        expect(gameState.getGameState).toBe(GAME_STATE.ACTIVE)
+    })
+
+    test('isGameActive method returns true when game is active', () => {
+        expect(new GameState(GAME_STATE.ACTIVE).isGameActive()).toBeTruthy()
+        expect(new GameState(GAME_STATE.GAME_SELECT).isGameActive()).toBeFalsy()
+    })
+
+    test('isGameInactive method returns true when game is inActive or paused', () => {
+        expect(new GameState(GAME_STATE.INACTIVE).isGameInactive()).toBeTruthy()
+        expect(new GameState(GAME_STATE.ACTIVE).isGameInactive()).toBeFalsy()
+    })
+
+    test('isGameSolved method returns true when game is over and is solved', () => {
+        expect(new GameState(GAME_STATE.OVER.SOLVED).isGameSolved()).toBeTruthy()
+        expect(new GameState(GAME_STATE.ACTIVE).isGameSolved()).toBeFalsy()
+    })
+
+    test('isGameUnsolved method returns true when game is over and is unsolved', () => {
+        expect(new GameState(GAME_STATE.OVER.UNSOLVED).isGameUnsolved()).toBeTruthy()
+        expect(new GameState(GAME_STATE.OVER.SOLVED).isGameUnsolved()).toBeFalsy()
+    })
+
+    test('isGameSelecting method returns true when use is choosing game', () => {
+        expect(new GameState(GAME_STATE.GAME_SELECT).isGameSelecting()).toBeTruthy()
+        expect(new GameState(GAME_STATE.INACTIVE).isGameSelecting()).toBeFalsy()
+    })
+
+})
---
+++ src/apps/arena/utils/classes/gameState.js
@@ -0,0 +1,49 @@
+import { GAME_STATE } from "../../../../resources/constants"
+
+export class GameState {
+    #state
+    constructor(value) {
+        if (GameState.isValidValue(value)) {
+            this.#state = value
+        } else {
+            throw new Error(`<${value}> is invalid value for Game State`);
+        }
+    }
+
+    static isValidValue(value) {
+        const validValues = [
+            GAME_STATE.ACTIVE,
+            GAME_STATE.INACTIVE,
+            GAME_STATE.DISPLAY_HINT,
+            GAME_STATE.GAME_SELECT,
+            GAME_STATE.OVER.SOLVED,
+            GAME_STATE.OVER.UNSOLVED,
+        ]
+        return validValues.includes(value)
+    }
+
+    get getGameState() {
+        return this.#state
+    }
+
+    isGameActive() {
+        return this.#state === GAME_STATE.ACTIVE
+    }
+
+    isGameInactive() {
+        return this.#state === GAME_STATE.INACTIVE
+    }
+
+    isGameSolved() {
+        return this.#state === GAME_STATE.OVER.SOLVED
+    }
+
+    isGameUnsolved() {
+        return this.#state === GAME_STATE.OVER.UNSOLVED
+    }
+
+    isGameSelecting() {
+        return this.#state === GAME_STATE.GAME_SELECT
+    }
+
+}
