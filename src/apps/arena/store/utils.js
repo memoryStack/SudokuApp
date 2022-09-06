5816f1029cf52a6444ff0c1f456203782d0daea0
Anuj Rao <anujabo186@gmail.com>
2022-09-06 21:54:05 +0530

    added TODO: for using redux data from actionHandler files

--- src/apps/arena/store/utils.js
+++ src/apps/arena/store/utils.js
@@ -3,6 +3,7 @@ import { GameState } from '../utils/classes/gameState'
 import { getGameState } from './selectors/gameState.selectors'
 
 // TODO: remove this function in immediate future
+// TODO: think over this way of using redux data
 export const isGameActive = () => {
     return new GameState(getGameState(getStoreState())).isGameActive()
 }
