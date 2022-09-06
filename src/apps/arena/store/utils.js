f2d2436b6e15e49b135730b21e00e281d5481128
Anuj Rao <anujabo186@gmail.com>
2022-09-06 21:13:38 +0530

    use gameState class for query related to gameState

--- src/apps/arena/store/utils.js
+++ src/apps/arena/store/utils.js
@@ -1,8 +1,8 @@
 import { getStoreState } from '../../../redux/dispatch.helpers'
-import { GAME_STATE } from '../../../resources/constants'
+import { GameState } from '../utils/classes/gameState'
 import { getGameState } from './selectors/gameState.selectors'
 
+// TODO: remove this function in immediate future
 export const isGameActive = () => {
-    const gameState = getGameState(getStoreState())
-    return gameState === GAME_STATE.ACTIVE
+    return new GameState(getGameState(getStoreState())).isGameActive()
 }
