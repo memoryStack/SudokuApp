1b62be4dacb00cc0b28d9ab49ba9996bd53daa43
Anuj Rao <anujabo186@gmail.com>
2022-11-19 14:13:26 +0530

    add a Houses class to hold some utils functions for all houses

---
+++ src/apps/arena/utils/classes/houses.js
@@ -0,0 +1,17 @@
+import { HOUSE_TYPE } from '../smartHints/constants'
+
+export class Houses {
+
+    static isRowHouse(houseType) {
+        return houseType === HOUSE_TYPE.ROW
+    }
+
+    static isColHouse(houseType) {
+        return houseType === HOUSE_TYPE.COL
+    }
+
+    static isBlockHouse(houseType) {
+        return houseType === HOUSE_TYPE.BLOCK
+    }
+
+}
