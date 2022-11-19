e16a3d83e6b2f94d8ca2a4903ef71bd486783028
Anuj Rao <anujabo186@gmail.com>
2022-11-19 20:11:20 +0530

    rename some variables

--- src/apps/arena/utils/smartHints/nakedGroup/nakedGroup.js
+++ src/apps/arena/utils/smartHints/nakedGroup/nakedGroup.js
@@ -1,4 +1,5 @@
 import _filter from 'lodash/src/utils/filter'
+import _map from 'lodash/src/utils/map'
 
 import { N_CHOOSE_K } from '../../../../../resources/constants'
 import { consoleLog, inRange } from '../../../../../utils/util'
@@ -30,6 +31,10 @@ import { getHouseCells } from '../../houseCells'
 // TODO: fix this parsing issue. at a lot of places we are
 // parsing the groupCandidates into their int form
 
+// TODO: move these constants 
+const VALID_CELL_MINIMUM_NOTES_COUNT = 2
+const MAX_VALID_CELLS_COUNT = 6
+
 // this func is used for a very special case in below func
 const getHouseCellsNum = (cells, houseType) => {
     let result = []
@@ -50,7 +55,6 @@ export const filterValidCellsInHouse = (house, groupCandidatesCount, mainNumbers
     return _filter(getHouseCells(house), (cell) => {
         if (!isCellEmpty(cell, mainNumbers)) return false
 
-        const VALID_CELL_MINIMUM_NOTES_COUNT = 2
         return inRange(
             getCellVisibleNotesCount(notesData[cell.row][cell.col]),
             { start: VALID_CELL_MINIMUM_NOTES_COUNT, end: groupCandidatesCount, }
@@ -58,9 +62,7 @@ export const filterValidCellsInHouse = (house, groupCandidatesCount, mainNumbers
     })
 }
 
-// TODO: break this file
 // TODO: think over the namings harder. i see a lot of in-consistencies
-// around 200 lines long function 
 export const highlightNakedDoublesOrTriples = (groupCandidatesCount, notesData, mainNumbers, maxHintsThreshold) => {
     const houseType = [HOUSE_TYPE.BLOCK, HOUSE_TYPE.ROW, HOUSE_TYPE.COL]
 
@@ -75,32 +77,26 @@ export const highlightNakedDoublesOrTriples = (groupCandidatesCount, notesData, 
     hintsSearchLoop: for (let j = 0; j < houseType.length; j++) {
         for (let houseNum = 0; houseNum < HOUSES_COUNT; houseNum++) {
             const house = { type: houseType[j], num: houseNum }
-            const houseAllBoxes = getHouseCells(house)
+            const houseAllCells = getHouseCells(house)
+            const validCells = filterValidCellsInHouse(house, groupCandidatesCount, mainNumbers, notesData)
 
-            const validBoxes = filterValidCellsInHouse(house, groupCandidatesCount, mainNumbers, notesData)
+            // to avoid computing 7C2 and 7C3, because that might be heavy but it's open for research
+            if (!inRange(validCells.length, { start: groupCandidatesCount, end: MAX_VALID_CELLS_COUNT })) continue
 
-            const validBoxesCount = validBoxes.length
-            // TODO: check the below threshold for naked multiple cases.
-            const VALID_BOXES_COUNT_THRESHOLD_TO_SEARCH = 6 // to avoid computing 7C2 and 7C3, because that might be heavy
-            if (validBoxesCount > VALID_BOXES_COUNT_THRESHOLD_TO_SEARCH || validBoxesCount < groupCandidatesCount) continue
-            const possibleSelections = N_CHOOSE_K[validBoxesCount][groupCandidatesCount]
+            const possibleSelections = N_CHOOSE_K[validCells.length][groupCandidatesCount]
 
             for (let k = 0; k < possibleSelections.length; k++) {
+                // finding for new conbination of cells or numbers
                 if (maxHintsLimitReached(hints, maxHintsThreshold)) {
                     break hintsSearchLoop
                 }
 
-                const selectedBoxes = []
-                for (let x = 0; x < possibleSelections[k].length; x++) {
-                    const selectedIndex = possibleSelections[k][x]
-                    const box = validBoxes[selectedIndex]
-                    selectedBoxes.push(box)
-                }
+                const selectedCells = _map(possibleSelections[k], (selectionIndex) => validCells[selectionIndex])
 
                 // check these boxes if they are covered or not already
                 if (Houses.isRowHouse(houseType[j]) || Houses.isColHouse(houseType[j])) {
                     // TODO: let's wrap this condition into a func
-                    const selectedCellsNum = getHouseCellsNum(selectedBoxes, houseType[j])
+                    const selectedCellsNum = getHouseCellsNum(selectedCells, houseType[j])
                     const houseCellsProcessed = groupsFoundInHouses[houseType[j]][`${houseNum}`] || []
                     let cellsProcessedAlready = houseCellsProcessed.length === selectedCellsNum.length
                     // run loop and check one by one
@@ -110,8 +106,8 @@ export const highlightNakedDoublesOrTriples = (groupCandidatesCount, notesData, 
                 }
 
                 const eachVisibleNotesInfo = {} // will store the visible notes info from all the selected boxes
-                for (let x = 0; x < selectedBoxes.length; x++) {
-                    const { row, col } = selectedBoxes[x]
+                for (let x = 0; x < selectedCells.length; x++) {
+                    const { row, col } = selectedCells[x]
                     for (let note = 1; note <= NUMBERS_IN_HOUSE; note++) {
                         if (notesData[row][col][note - 1].show) {
                             if (!eachVisibleNotesInfo[note]) eachVisibleNotesInfo[note] = 1
@@ -134,34 +130,34 @@ export const highlightNakedDoublesOrTriples = (groupCandidatesCount, notesData, 
                 if (shouldAbort) continue // analyze some other combination of cells
 
                 // if house is row or col
-                if ((Houses.isRowHouse(houseType[j]) || Houses.isColHouse(houseType[j])) && areSameBlockCells(selectedBoxes)) {
-                    const { blockNum } = getBlockAndBoxNum(selectedBoxes[0])
+                if ((Houses.isRowHouse(houseType[j]) || Houses.isColHouse(houseType[j])) && areSameBlockCells(selectedCells)) {
+                    const { blockNum } = getBlockAndBoxNum(selectedCells[0])
                     for (let boxNum = 0; boxNum < CELLS_IN_HOUSE; boxNum++) {
                         const { row, col } = getRowAndCol(blockNum, boxNum)
                         if (
                             (Houses.isRowHouse(houseType[j]) && row !== houseNum) ||
                             (Houses.isColHouse(houseType[j]) && col !== houseNum)
                         )
-                            houseAllBoxes.push({ row, col })
+                            houseAllCells.push({ row, col })
                     }
                 } else {
-                    if (areSameRowCells(selectedBoxes)) {
-                        const { row } = selectedBoxes[0]
+                    if (areSameRowCells(selectedCells)) {
+                        const { row } = selectedCells[0]
                         for (let col = 0; col < CELLS_IN_HOUSE; col++) {
                             const { blockNum } = getBlockAndBoxNum({ row, col })
-                            if (houseNum !== blockNum) houseAllBoxes.push({ row, col })
+                            if (houseNum !== blockNum) houseAllCells.push({ row, col })
                         }
-                    } else if (areSameColCells(selectedBoxes)) {
-                        const { col } = selectedBoxes[0]
+                    } else if (areSameColCells(selectedCells)) {
+                        const { col } = selectedCells[0]
                         for (let row = 0; row < CELLS_IN_HOUSE; row++) {
                             const { blockNum } = getBlockAndBoxNum({ row, col })
-                            if (houseNum !== blockNum) houseAllBoxes.push({ row, col })
+                            if (houseNum !== blockNum) houseAllCells.push({ row, col })
                         }
                     }
                 }
 
-                const groupWillRemoveCandidates = houseAllBoxes.some(cell => {
-                    const isSelectedCell = selectedBoxes.some(selectedCell => {
+                const groupWillRemoveCandidates = houseAllCells.some(cell => {
+                    const isSelectedCell = selectedCells.some(selectedCell => {
                         return areSameCells(cell, selectedCell)
                     })
                     return (
@@ -176,15 +172,15 @@ export const highlightNakedDoublesOrTriples = (groupCandidatesCount, notesData, 
                 if (!groupWillRemoveCandidates) continue
 
                 // Note: the correctness of this DS depends on entries order in "houseType"
-                groupsFoundInHouses[houseType[j]][`${houseNum}`] = getHouseCellsNum(selectedBoxes, houseType[j])
-                if (houseAllBoxes.length === 15) {
+                groupsFoundInHouses[houseType[j]][`${houseNum}`] = getHouseCellsNum(selectedCells, houseType[j])
+                if (houseAllCells.length === 15) {
                     // group cells belong to 2 houses, one is block for sure and another one can be either row or col
-                    if (areSameRowCells(selectedBoxes)) {
-                        const { row } = selectedBoxes[0]
-                        groupsFoundInHouses['row'][`${row}`] = getHouseCellsNum(selectedBoxes, 'row')
+                    if (areSameRowCells(selectedCells)) {
+                        const { row } = selectedCells[0]
+                        groupsFoundInHouses['row'][`${row}`] = getHouseCellsNum(selectedCells, 'row')
                     } else {
-                        const { col } = selectedBoxes[0]
-                        groupsFoundInHouses['col'][`${col}`] = getHouseCellsNum(selectedBoxes, 'col')
+                        const { col } = selectedCells[0]
+                        groupsFoundInHouses['col'][`${col}`] = getHouseCellsNum(selectedCells, 'col')
                     }
                 }
 
@@ -192,17 +188,17 @@ export const highlightNakedDoublesOrTriples = (groupCandidatesCount, notesData, 
                     type: GROUPS.NAKED_GROUP,
                     data: {
                         groupCandidates: groupCandidates.map(candidate => parseInt(candidate, 10)),
-                        hostCells: selectedBoxes,
+                        hostCells: selectedCells,
                     },
                 })
                 if (isValidNakedGroup) {
-                    consoleLog('@@@@ naked group', selectedBoxes, groupCandidates)
+                    consoleLog('@@@@ naked group', selectedCells, groupCandidates)
                     const groupCandidatesInInt = groupCandidates.map(groupCandidate => parseInt(groupCandidate, 10))
                     hints.push(
                         prepareNakedDublesOrTriplesHintData(
                             groupCandidatesCount,
-                            houseAllBoxes,
-                            selectedBoxes,
+                            houseAllCells,
+                            selectedCells,
                             groupCandidatesInInt,
                             notesData,
                         ),
