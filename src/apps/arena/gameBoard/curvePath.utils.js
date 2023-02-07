08be9106e99dfa2a9d58d2d0115883e192f8d968
Anuj Rao <anujabo186@gmail.com>
2023-02-07 13:33:13 +0530

    Add util for finding points on a line at certain distances from endpoints

--- src/apps/arena/gameBoard/curvePath.utils.js
+++ src/apps/arena/gameBoard/curvePath.utils.js
@@ -20,3 +20,24 @@ export const getRoatatedPoint = (point, pivot, angleInDegree) => {
         y: rotatedShiftedPoint.y + pivot.y,
     }
 }
+
+export const getPointsOnLineFromEndpoints = (line, distanceOnLine) => {
+    const { start, end } = line
+
+    const segmentLength = Math.sqrt((start.x - end.x) ** 2 + (start.y - end.y) ** 2)
+
+    const closeToStart = {
+        x: start.x + ((end.x - start.x) * distanceOnLine) / segmentLength,
+        y: start.y + ((end.y - start.y) * distanceOnLine) / segmentLength,
+    }
+
+    const closeToEnd = {
+        x: end.x - ((end.x - start.x) * distanceOnLine) / segmentLength,
+        y: end.y - ((end.y - start.y) * distanceOnLine) / segmentLength,
+    }
+
+    return {
+        closeToStart,
+        closeToEnd,
+    }
+}
--- src/apps/arena/gameBoard/curvePath.utils.test.js
+++ src/apps/arena/gameBoard/curvePath.utils.test.js
@@ -1,4 +1,4 @@
-import { getRoatatedPoint } from './curvePath.utils'
+import { getRoatatedPoint, getPointsOnLineFromEndpoints } from './curvePath.utils'
 
 describe('getRoatatedPoint()', () => {
     test('rotates point anticlockwise w.r.t origin', () => {
@@ -43,5 +43,51 @@ describe('getRoatatedPoint()', () => {
         const rotatedPoint = getRoatatedPoint(point, pivot, angleInDegree)
         expect(rotatedPoint.x).toBeCloseTo(expectedPoint.x, 3)
         expect(rotatedPoint.y).toBeCloseTo(expectedPoint.y, 5)
+    })
+})
+
+describe('getPointsOnLineFromEndpoints()', () => {
+    test('returns points close to both endpoints on horizontal line', () => {
+        const line = {
+            start: { x: 0, y: 0 },
+            end: { x: 4, y: 0 },
+        }
+        const distance = 1
+        const expectedResult = {
+            closeToStart: { x: 1, y: 0 },
+            closeToEnd: { x: 3, y: 0 },
+        }
+        expect(getPointsOnLineFromEndpoints(line, distance)).toEqual(expectedResult)
+    })
+
+    test('returns points close to both endpoints on vertical line', () => {
+        const line = {
+            start: { x: 4, y: 0 },
+            end: { x: 4, y: 6 },
+        }
+        const distance = 1
+        const expectedResult = {
+            closeToStart: { x: 4, y: 1 },
+            closeToEnd: { x: 4, y: 5 },
+        }
+        expect(getPointsOnLineFromEndpoints(line, distance)).toEqual(expectedResult)
+    })
+
+    test('works fine for general line as well', () => {
+        const line = {
+            end: { x: -1, y: -1 },
+            start: { x: 3, y: 7 },
+        }
+        const distance = 2.2360679775
+        const expectedResult = {
+            closeToStart: { x: 2, y: 5 },
+            closeToEnd: { x: 0, y: 1 },
+        }
+
+        const returnedResult = getPointsOnLineFromEndpoints(line, distance)
+        expect(returnedResult.closeToStart.x).toBeCloseTo(expectedResult.closeToStart.x, 5)
+        expect(returnedResult.closeToStart.y).toBeCloseTo(expectedResult.closeToStart.y, 5)
+        expect(returnedResult.closeToEnd.y).toBeCloseTo(expectedResult.closeToEnd.y, 5)
+        expect(returnedResult.closeToEnd.x).toBeCloseTo(expectedResult.closeToEnd.x, 5)
     })
 })
