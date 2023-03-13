61ab76854a237e151a95ffd424b1937c131a2a25
Anuj Rao <anujabo186@gmail.com>
2023-03-13 09:58:44 +0530

    Add default language until i18n is not initialized

--- src/i18n/hooks/useTranslation.js
+++ src/i18n/hooks/useTranslation.js
@@ -1,11 +1,13 @@
 import { useTranslation as externalUseTranslation } from 'react-i18next'
 
+import { LANGUAGE_KEYS } from '../languages'
+
 // just a wrapper around the external dependency
 export const useTranslation = () => {
     const result = externalUseTranslation()
 
     return {
         ...result,
-        selectedLanguage: result?.i18n?.language,
+        selectedLanguage: result?.i18n?.language || LANGUAGE_KEYS.ENGLISH,
     }
 }
