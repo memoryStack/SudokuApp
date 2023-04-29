cece1fc7f4fd88921822ca5c6b48ace17c1ee46c
Anuj Rao <anujabo186@gmail.com>
2023-04-29 19:56:29 +0530

    Add tokens for shape and border radiuses

---
+++ src/designSystem/tokens/borderRadius.js
@@ -0,0 +1,32 @@
+module.exports = {
+    borderRadius: {
+        none: {
+            value: 0,
+            isCore: true,
+        },
+        'extra-small': {
+            value: 4,
+            isCore: true,
+        },
+        small: {
+            value: 8,
+            isCore: true,
+        },
+        medium: {
+            value: 12,
+            isCore: true,
+        },
+        large: {
+            value: 16,
+            isCore: true,
+        },
+        'extra-large': {
+            value: 28,
+            isCore: true,
+        },
+        full: {
+            value: '100%',
+            isCore: true,
+        },
+    },
+}
---
+++ src/designSystem/tokens/shape.js
@@ -0,0 +1,76 @@
+module.exports = {
+    shape: {
+        corner: {
+            none: {
+                value: {
+                    borderRadius: '{borderRadius.none}',
+                },
+            },
+            'extra-small': {
+                value: {
+                    borderRadius: '{borderRadius.extra-small}',
+                },
+            },
+            'extra-small-top': {
+                value: {
+                    borderTopLeftRadius: '{borderRadius.extra-small}',
+                    borderTopRightRadius: '{borderRadius.extra-small}',
+                    borderBottomRightRadius: '{borderRadius.none}',
+                    borderBottomLeftRadius: '{borderRadius.none}',
+                },
+            },
+            small: {
+
+                value: {
+                    borderRadius: '{borderRadius.small}',
+                },
+
+            },
+            medium: {
+                value: {
+                    borderRadius: '{borderRadius.medium}',
+                },
+            },
+            large: {
+                value: {
+                    borderRadius: '{borderRadius.large}',
+                },
+            },
+            'large-end': {
+                value: {
+                    borderTopLeftRadius: '{borderRadius.none}',
+                    borderTopRightRadius: '{borderRadius.large}',
+                    borderBottomRightRadius: '{borderRadius.large}',
+                    borderBottomLeftRadius: '{borderRadius.none}',
+                },
+            },
+            'large-top': {
+                value: {
+                    borderTopLeftRadius: '{borderRadius.large}',
+                    borderTopRightRadius: '{borderRadius.large}',
+                    borderBottomRightRadius: '{borderRadius.none}',
+                    borderBottomLeftRadius: '{borderRadius.none}',
+                },
+            },
+            'extra-large': {
+                value: {
+                    borderRadius: '{borderRadius.extra-large}',
+                },
+            },
+            'extra-large-top': {
+                value: {
+                    borderTopLeftRadius: '{borderRadius.extra-large}',
+                    borderTopRightRadius: '{borderRadius.extra-large}',
+                    borderBottomRightRadius: '{borderRadius.none}',
+                    borderBottomLeftRadius: '{borderRadius.none}',
+                },
+            },
+            full: {
+                value: {
+                    borderRadius: '{borderRadius.full}',
+                },
+            },
+        },
+
+    },
+}
