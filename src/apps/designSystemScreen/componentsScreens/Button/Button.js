7dddebb699307b33cc4a995d3789a3d398f84288
Anuj Rao <anujabo186@gmail.com>
2023-05-14 19:49:25 +0530

    Add Button disabled variants

--- src/apps/designSystemScreen/componentsScreens/Button/Button.js
+++ src/apps/designSystemScreen/componentsScreens/Button/Button.js
@@ -4,7 +4,7 @@ import { View } from 'react-native'
 
 import _map from '@lodash/map'
 
-import Button, { BUTTON_TYPES } from '@ui/molecules/Button'
+import Button, { BUTTON_STATES, BUTTON_TYPES } from '@ui/molecules/Button'
 
 import { styles } from './button.styles'
 
@@ -14,20 +14,35 @@ const BUTTONS = [
         type: BUTTON_TYPES.FILLED,
     },
     {
+        label: 'Filled Button Disabled',
+        type: BUTTON_TYPES.FILLED,
+        state: BUTTON_STATES.DISABLED,
+    },
+    {
         label: 'Tonal Button',
         type: BUTTON_TYPES.TONAL,
+    },
+    {
+        label: 'Tonal Button Disabled',
+        type: BUTTON_TYPES.TONAL,
+        state: BUTTON_STATES.DISABLED,
     },
     {
         label: 'Text Button',
         type: BUTTON_TYPES.TEXT,
     },
+    {
+        label: 'Text Button Disabled',
+        type: BUTTON_TYPES.TEXT,
+        state: BUTTON_STATES.DISABLED,
+    },
 ]
 
 const ButtonDemo = () => {
     // TODO: remove this extra view and add this support in Button only
-    const renderItem = ({ label, type }) => (
+    const renderItem = ({ label, type, ...rest }) => (
         <View style={{ marginTop: 40 }} key={type}>
-            <Button type={type} label={label} />
+            <Button type={type} label={label} {...rest} />
         </View>
     )
 
