e2bfc49edd5832bc2744440b5e62e8991fb61bc0
Anuj Rao <anujabo186@gmail.com>
2023-05-21 18:56:45 +0530

    Add tokens for Divider component

---
+++ src/designSystem/tokens/divider.js
@@ -0,0 +1,12 @@
+// https://m3.material.io/components/divider/specs
+// haven't added tokens mentioned in above article related to insets and margins
+module.exports = {
+    divider: {
+        color: {
+            value: '{colors.outline-variant}',
+        },
+        thickness: {
+            value: 1,
+        },
+    },
+}
