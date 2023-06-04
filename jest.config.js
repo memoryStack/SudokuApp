88702bd145c40b8699a631a00f45fa7ef100ceb3
Anuj Rao <anujabo186@gmail.com>
2023-06-04 11:47:32 +0530

    Use RNGesture mock from it's node_modules

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     preset: 'react-native',
+    setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
     transformIgnorePatterns: [],
     setupFilesAfterEnv: [
         '@testing-library/jest-native/extend-expect',
         '<rootDir>/jestSetupFiles/mockRNSwitchComponent.js',
-        '<rootDir>/jestSetupFiles/gestureMock.js',
     ],
 }
