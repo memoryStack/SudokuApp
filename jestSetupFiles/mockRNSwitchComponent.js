8c4deb83073cc84c8e2a50609ad019b6015987d6
Anuj Rao <anujabo186@gmail.com>
2023-06-10 13:28:04 +0530

    Avoid useNativeDriver warning for jest

--- jestSetupFiles/mockRNSwitchComponent.js
+++ jestSetupFiles/mockRNSwitchComponent.js
@@ -4,3 +4,7 @@ jest.mock('react-native/Libraries/Components/Switch/Switch', () => {
         default: mockComponent('react-native/Libraries/Components/Switch/Switch'),
     }
 })
+
+// TODO: put it in separate file
+// added it to avoid Animated useNativeDriver warning
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
