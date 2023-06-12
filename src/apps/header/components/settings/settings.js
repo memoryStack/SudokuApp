b60e096e427cc08a15d003fca14c1b000b8300aa
Anuj Rao <anujabo186@gmail.com>
2023-06-12 22:28:53 +0530

    Remove setting onPress handler from settings file when we can use it locally

--- src/apps/header/components/settings/settings.js
+++ src/apps/header/components/settings/settings.js
@@ -1,43 +1,36 @@
 import React, {
-    memo, useEffect, useRef,
+    memo, useRef,
 } from 'react'
 
 import { View, Text } from 'react-native'
 
 import _map from '@lodash/map'
 
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { SettingsIcon } from '@resources/svgIcons/settings'
 
 import withActions from '@utils/hocs/withActions'
+
 import { useModal } from 'src/apps/arena/hooks/useModal'
-import { SettingsIcon } from '@resources/svgIcons/settings'
+
 import { useToggle } from '../../../../utils/customHooks'
-import { HEADER_ITEMS, HEADER_ITEMS_PRESS_HANDLERS_KEYS } from '../../../../navigation/route.constants'
 
 import { Touchable, TouchableTypes } from '../../../components/Touchable'
 
-import { MENU_ITEMS, SETTINGS_BUTTON_TEST_ID, SETTINGS_MENU_TEST_ID } from './settings.constants'
-
 import { ACTION_HANDLERS, ACTION_TYPES } from './settings.actionHandlers'
+import { MENU_ITEMS, SETTINGS_BUTTON_TEST_ID, SETTINGS_MENU_TEST_ID } from './settings.constants'
 import { styles } from './settings.style'
 
-export const Settings_ = ({ navigation, onPress, onAction }) => {
+export const Settings_ = ({ onAction }) => {
     const modalContextValues = useModal()
 
     const [openMenu, toggleMenuVisibility] = useToggle(false)
 
     const iconRef = useRef(null)
 
-    useEffect(() => {
-        navigation.setParams({
-            [HEADER_ITEMS_PRESS_HANDLERS_KEYS[HEADER_ITEMS.SETTINGS]]: toggleMenuVisibility,
-        })
-    }, [navigation])
-
     const renderIcon = () => (
         <Touchable
             touchable={TouchableTypes.opacity}
-            onPress={onPress}
+            onPress={toggleMenuVisibility}
             testID={SETTINGS_BUTTON_TEST_ID}
         >
             <View ref={iconRef} collapsable={false}>
@@ -60,15 +53,16 @@ export const Settings_ = ({ navigation, onPress, onAction }) => {
         return (
             <View style={styles.menuContainer} testID={SETTINGS_MENU_TEST_ID}>
                 {_map(MENU_ITEMS, (item, index) => (
-                    <TouchableOpacity
+                    <Touchable
                         key={item.label}
-                        touchable={TouchableTypes.withoutFeedBack}
+                        touchable={TouchableTypes.opacity}
                         onPress={() => onItemPress(item)}
+                        avoidDefaultStyles
                     >
                         <Text style={[styles.menuText, index ? styles.spaceBetweenMenuItems : null]}>
                             {item.label}
                         </Text>
-                    </TouchableOpacity>
+                    </Touchable>
                 ))}
             </View>
         )
