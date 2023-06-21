db031910b2c709afb548008db47dda90239555b9
Anuj Rao <anujabo186@gmail.com>
2023-06-21 23:25:42 +0530

    Refactore Touchable component

--- src/apps/components/Touchable.js
+++ src/apps/components/Touchable.js
@@ -9,8 +9,6 @@ import {
 
 import PropTypes from 'prop-types'
 
-// import { TouchableOpacity } from 'react-native-gesture-handler'
-
 import { Platform } from '../../utils/classes/platform'
 // import { TouchableNativeFeedback as NewTouchableNativeFeedback } from 'react-native-gesture-handler'
 // will fix error related to this later
@@ -22,7 +20,6 @@ const styles = StyleSheet.create({
     },
 })
 
-// all touchables
 export const TouchableTypes = {
     nativeFeedBack: 'nativeFeedBack',
     highLight: 'highLight',
@@ -41,32 +38,39 @@ const TouchablesMap = {
 
 const defaultTouchable = TouchableTypes.opacity
 
-const getTouchable = touchable => {
-    if (!touchable) touchable = defaultTouchable
-    touchable = touchable.toLowerCase().includes('nativefeedback') && Platform.isIOS() ? 'highLight' : touchable
-    return TouchablesMap[touchable]
+const getFinalTouchableType = touchableFromProps => {
+    if (!touchableFromProps) return defaultTouchable
+    return touchableFromProps.toLowerCase().includes('nativefeedback') && Platform.isIOS() ? 'highLight' : touchableFromProps
 }
+
+const getTouchable = touchable => TouchablesMap[getFinalTouchableType(touchable)]
 
 export const Touchable = props => {
     const {
-        style, touchable, underlayColor = 'white', children, avoidDefaultStyles, addHitSlop, ...rest
+        style,
+        touchable,
+        underlayColor = 'white',
+        children,
+        avoidDefaultStyles,
+        addHitSlop,
+        ...rest
     } = props
 
     const hitSlop = addHitSlop ? {
         top: 16, bottom: 16, left: 16, right: 16,
     } : null
 
-    const Touchable = getTouchable(touchable)
+    const TouchableComponent = getTouchable(touchable)
 
     return (
-        <Touchable
+        <TouchableComponent
             style={[avoidDefaultStyles ? null : styles.container, style]}
             underlayColor={underlayColor}
             hitSlop={hitSlop}
             {...rest}
         >
             {children}
-        </Touchable>
+        </TouchableComponent>
     )
 }
 
