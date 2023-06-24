c91c9d6c1bfb55c4af88036d0c2e2bf7715c1f2f
Anuj Rao <anujabo186@gmail.com>
2023-06-24 13:21:06 +0530

    Group arena game pause test cases

--- src/apps/arena/__tests__/arena.test.js
+++ src/apps/arena/__tests__/arena.test.js
@@ -15,6 +15,15 @@ import {
 import { ARENA_PAGE_TEST_ID } from '../constants'
 import { TIMER_START_ICON_TEST_ID, TIMER_TEST_ID } from '../timer/timer.constants'
 
+const renderScreenAndWaitForPuzzleStart = async () => {
+    renderScreen({
+        route: ROUTES.ARENA,
+        getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
+    })
+
+    await hasPuzzleStarted()
+}
+
 describe('Arena Screen', () => {
     // added this to avoid "ReferenceError: You are trying to `import` a file after the Jest environment has been torn down."
     // error due to timer setTimeout
@@ -25,84 +34,56 @@ describe('Arena Screen', () => {
     afterEach(cleanup)
 
     test('should go back to home page on back button press', async () => {
-        renderScreen({
-            route: ROUTES.ARENA,
-            getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
-        })
-
-        await hasPuzzleStarted()
+        await renderScreenAndWaitForPuzzleStart()
 
         fireEvent.press(screen.getByTestId(HEADER_ITEM_VS_TEST_ID[HEADER_ITEMS.BACK]))
 
         expect(getScreenName()).toBe(ROUTES.HOME)
     })
+})
 
-    // game pause attribute 1
-    test('on clicking on Timer all board numbers will be disappeared', async () => {
-        renderScreen({
-            route: ROUTES.ARENA,
-            getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
-        })
+describe('Game Pause', () => {
+    beforeEach(() => {
+        jest.useFakeTimers()
+    })
+    afterEach(cleanup)
 
-        await hasPuzzleStarted()
+    test('all board numbers will be disappeared', async () => {
+        await renderScreenAndWaitForPuzzleStart()
 
         fireEvent.press(screen.getByTestId(TIMER_TEST_ID))
 
         checkAllCellsEmpty()
     })
 
-    // game pause attribute 2
-    test('all board controllers will be disabled on timer click', async () => {
-        renderScreen({
-            route: ROUTES.ARENA,
-            getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
-        })
-
-        await hasPuzzleStarted()
+    test('all board controllers will be disabled', async () => {
+        await renderScreenAndWaitForPuzzleStart()
 
         fireEvent.press(screen.getByTestId(TIMER_TEST_ID))
 
         allBoardControllersDisabled()
     })
 
-    // game pause attribute 3
-    test('all input panel items will be disabled on timer click', async () => {
-        renderScreen({
-            route: ROUTES.ARENA,
-            getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
-        })
-
-        await hasPuzzleStarted()
+    test('all input panel items will be disabled', async () => {
+        await renderScreenAndWaitForPuzzleStart()
 
         fireEvent.press(screen.getByTestId(TIMER_TEST_ID))
 
         allInputPanelItemsAreDisabled()
     })
 
-    // game pause attribute 4
-    test('start the timer icon will be visible on timer click', async () => {
-        renderScreen({
-            route: ROUTES.ARENA,
-            getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
-        })
-
-        await hasPuzzleStarted()
+    test('start the timer icon will be visible', async () => {
+        await renderScreenAndWaitForPuzzleStart()
 
         fireEvent.press(screen.getByTestId(TIMER_TEST_ID))
 
         expect(screen.getByTestId(TIMER_START_ICON_TEST_ID)).toBeVisible()
     })
 
-    // game pause attribute 5
-    test('timer will be cleared on timer click', async () => {
+    test('timer will be cleared', async () => {
         jest.spyOn(global, 'clearInterval')
 
-        renderScreen({
-            route: ROUTES.ARENA,
-            getScreenRootElement: () => screen.getByTestId(ARENA_PAGE_TEST_ID),
-        })
-
-        await hasPuzzleStarted()
+        await renderScreenAndWaitForPuzzleStart()
 
         fireEvent.press(screen.getByTestId(TIMER_TEST_ID))
 
