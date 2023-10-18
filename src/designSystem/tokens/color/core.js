71ea75caf4ee2f385b3bf5c1b6b1dd79c0b2a5ff
memoryStack <anujabo186@gmail.com>
2023-10-18 18:55:22 +0530

    Update color variations algo

--- src/designSystem/tokens/color/core.js
+++ src/designSystem/tokens/color/core.js
@@ -4,267 +4,128 @@
 // // take hue and chroma from themebuilder (https://m3.material.io/theme-builder#/custom)
 // // and just change the lightness as per the variant is required
 
-// const color = Color({ r: 248, g: 200, b: 220 }).hex()
-// console.log(color)
-
 // const lightnessList = [10, 20, 30, 40, 50, 60, 70, 80, 87, 90, 92, 94, 95, 96, 98, 99]
+
+// const colorHSL = Color('#666666').hsl()
+
 // const result = {}
+
 // lightnessList.forEach(lightnessVale => {
 //     const color = Color({
-//         h: 176, // originalcolors.color[0],
-//         s: 45, // originalcolors.color[1],
+//         h: colorHSL.color[0],
+//         s: colorHSL.color[1],
 //         l: lightnessVale,
 //     })
 //     result[lightnessVale] = {
 //         value: color.hex(),
 //         isCore: true,
 //     }
-//     // console.log(`color: ${colors.hex()}; // lightness value is ${lightnessVale}`)
 // })
 // console.log(result)
 
 module.exports = {
     colors: {
         white: { value: '#FFFFFF', isCore: true },
-        black: {
-            value: '#000000',
-            isCore: true,
-        },
+        black: { value: '#000000', isCore: true },
         primary: {
-            0: {
-                value: '{colors.black}',
-                isCore: true,
-            },
-            10: {
-                value: '#21005D',
-                isCore: true,
-            },
-            20: {
-                value: '#381E72',
-                isCore: true,
-            },
-            30: {
-                value: '#4F378B',
-                isCore: true,
-            },
-            40: {
-                value: '#6750A4',
-                isCore: true,
-            },
-            50: {
-                value: '#7F67BE',
-                isCore: true,
-            },
-            60: {
-                value: '#9A82DB',
-                isCore: true,
-            },
-            70: {
-                value: '#B69DF8',
-                isCore: true,
-            },
-            80: {
-                value: '#D0BCFF',
-                isCore: true,
-            },
-            90: {
-                value: '#EADDFF',
-                isCore: true,
-            },
-            95: {
-                value: '#F6EDFF',
-                isCore: true,
-            },
-            99: {
-                value: '#FFFBFF',
-                isCore: true,
-            },
-            100: {
-                value: '{colors.white}',
-                isCore: true,
-            },
+            0: { value: '{colors.black}', isCore: true },
+            10: { value: '#140B28', isCore: true },
+            20: { value: '#281551', isCore: true },
+            30: { value: '#3C2079', isCore: true },
+            40: { value: '#4F2AA2', isCore: true },
+            50: { value: '#6335CA', isCore: true },
+            60: { value: '#825DD5', isCore: true },
+            70: { value: '#A286DF', isCore: true },
+            80: { value: '#C1AEEA', isCore: true },
+            87: { value: '#D6CBF1', isCore: true },
+            90: { value: '#E0D7F4', isCore: true },
+            92: { value: '#E6DFF6', isCore: true },
+            94: { value: '#ECE7F9', isCore: true },
+            95: { value: '#EFEBFA', isCore: true },
+            96: { value: '#F3EFFB', isCore: true },
+            98: { value: '#F9F7FD', isCore: true },
+            99: { value: '#FCFBFE', isCore: true },
+            100: { value: '{colors.white}', isCore: true },
         },
         secondary: {
-            0: {
-                value: '{colors.black}',
-                isCore: true,
-            },
-            10: {
-                value: '#1D192B',
-                isCore: true,
-            },
-            20: {
-                value: '#332D41',
-                isCore: true,
-            },
-            30: {
-                value: '#4A4458',
-                isCore: true,
-            },
-            40: {
-                value: '#625B71',
-                isCore: true,
-            },
-            50: {
-                value: '#7A7289',
-                isCore: true,
-            },
-            60: {
-                value: '#958DA5',
-                isCore: true,
-            },
-            70: {
-                value: '#B0A7C0',
-                isCore: true,
-            },
-            80: {
-                value: '#CCC2DC',
-                isCore: true,
-            },
-            90: {
-                value: '#E8DEF8',
-                isCore: true,
-            },
-            95: {
-                value: '#F6EDFF',
-                isCore: true,
-            },
-            99: {
-                value: '#FFFBFF',
-                isCore: true,
-            },
-            100: {
-                value: '{colors.white}',
-                isCore: true,
-            },
+            0: { value: '{colors.black}', isCore: true },
+            10: { value: '#18151E', isCore: true },
+            20: { value: '#2F2A3C', isCore: true },
+            30: { value: '#473F5A', isCore: true },
+            40: { value: '#5F5379', isCore: true },
+            50: { value: '#766897', isCore: true },
+            60: { value: '#9286AC', isCore: true },
+            70: { value: '#ADA5C0', isCore: true },
+            80: { value: '#C8C3D5', isCore: true },
+            87: { value: '#DBD8E4', isCore: true },
+            90: { value: '#E4E1EA', isCore: true },
+            92: { value: '#E9E7EE', isCore: true },
+            94: { value: '#EFEDF2', isCore: true },
+            95: { value: '#F1F0F5', isCore: true },
+            96: { value: '#F4F3F7', isCore: true },
+            98: { value: '#FAF9FB', isCore: true },
+            99: { value: '#FCFCFD', isCore: true },
+            100: { value: '{colors.white}', isCore: true },
         },
         tertiary: {
-            0: {
-                value: '{colors.black}',
-                isCore: true,
-            },
-            10: {
-                value: '#31111D',
-                isCore: true,
-            },
-            20: {
-                value: '#492532',
-                isCore: true,
-            },
-            30: {
-                value: '#633B48',
-                isCore: true,
-            },
-            40: {
-                value: '#7D5260',
-                isCore: true,
-            },
-            50: {
-                value: '#986977',
-                isCore: true,
-            },
-            60: {
-                value: '#B58392',
-                isCore: true,
-            },
-            70: {
-                value: '#D29DAC',
-                isCore: true,
-            },
-            80: {
-                value: '#EFB8C8',
-                isCore: true,
-            },
-            90: {
-                value: '#FFD8E4',
-                isCore: true,
-            },
-            95: {
-                value: '#FFECF1',
-                isCore: true,
-            },
-            99: {
-                value: '#FFFBFA',
-                isCore: true,
-            },
-            100: {
-                value: '{colors.white}',
-                isCore: true,
-            },
+            0: { value: '{colors.black}', isCore: true },
+            10: { value: '#201317', isCore: true },
+            20: { value: '#40262E', isCore: true },
+            30: { value: '#603946', isCore: true },
+            40: { value: '#804C5D', isCore: true },
+            50: { value: '#A05F74', isCore: true },
+            60: { value: '#B37F90', isCore: true },
+            70: { value: '#C69FAC', isCore: true },
+            80: { value: '#D9BFC7', isCore: true },
+            87: { value: '#E6D5DB', isCore: true },
+            90: { value: '#ECDFE3', isCore: true },
+            92: { value: '#F0E5E9', isCore: true },
+            94: { value: '#F4ECEE', isCore: true },
+            95: { value: '#F5EFF1', isCore: true },
+            96: { value: '#F7F2F4', isCore: true },
+            98: { value: '#FBF9F9', isCore: true },
+            99: { value: '#FDFCFC', isCore: true },
+            100: { value: '{colors.white}', isCore: true },
         },
         neutral: {
             0: { value: '{colors.black}', isCore: true },
-            10: { value: '#1B171C', isCore: true },
+            10: { value: '#1C181C', isCore: true },
             20: { value: '#372F37', isCore: true },
-            30: { value: '#524653', isCore: true },
+            30: { value: '#524752', isCore: true },
             40: { value: '#6E5E6E', isCore: true },
-            50: { value: '#89758A', isCore: true },
+            50: { value: '#8A768A', isCore: true },
             60: { value: '#A191A1', isCore: true },
-            70: { value: '#B8ACB9', isCore: true },
+            70: { value: '#B8ADB8', isCore: true },
             80: { value: '#D0C8D0', isCore: true },
-            87: { value: '#E0DBE1', isCore: true },
-            90: { value: '#E7E3E8', isCore: true },
+            87: { value: '#E0DBE0', isCore: true },
+            90: { value: '#E8E3E8', isCore: true },
             92: { value: '#ECE9EC', isCore: true },
-            94: { value: '#F1EEF1', isCore: true },
+            94: { value: '#F1EFF1', isCore: true },
             95: { value: '#F3F1F3', isCore: true },
             96: { value: '#F6F4F6', isCore: true },
-            98: { value: '#FAF9FA', isCore: true },
+            98: { value: '#FAFAFA', isCore: true },
             99: { value: '#FDFCFD', isCore: true },
             100: { value: '{colors.white}', isCore: true },
         },
         'neutral-variant': {
-            0: {
-                value: '{colors.black}',
-                isCore: true,
-            },
-            10: {
-                value: '#1D1A22',
-                isCore: true,
-            },
-            20: {
-                value: '#332F37',
-                isCore: true,
-            },
-            30: {
-                value: '#49454F',
-                isCore: true,
-            },
-            40: {
-                value: '#605D66',
-                isCore: true,
-            },
-            50: {
-                value: '#79747E',
-                isCore: true,
-            },
-            60: {
-                value: '#938F99',
-                isCore: true,
-            },
-            70: {
-                value: '#AEA9B4',
-                isCore: true,
-            },
-            80: {
-                value: '#CAC4D0',
-                isCore: true,
-            },
-            90: {
-                value: '#E7E0EC',
-                isCore: true,
-            },
-            95: {
-                value: '#F5EEFA',
-                isCore: true,
-            },
-            99: {
-                value: '#FFFBFE',
-                isCore: true,
-            },
-            100: {
-                value: '{colors.white}',
-                isCore: true,
-            },
+            0: { value: '{colors.black}', isCore: true },
+            10: { value: '#1A181C', isCore: true },
+            20: { value: '#332F37', isCore: true },
+            30: { value: '#4D4752', isCore: true },
+            40: { value: '#665E6E', isCore: true },
+            50: { value: '#7F768A', isCore: true },
+            60: { value: '#9991A1', isCore: true },
+            70: { value: '#B3ADB8', isCore: true },
+            80: { value: '#CCC8D0', isCore: true },
+            87: { value: '#DEDBE0', isCore: true },
+            90: { value: '#E6E3E8', isCore: true },
+            92: { value: '#EBE9EC', isCore: true },
+            94: { value: '#F0EFF1', isCore: true },
+            95: { value: '#F2F1F3', isCore: true },
+            96: { value: '#F5F4F6', isCore: true },
+            98: { value: '#FAFAFA', isCore: true },
+            99: { value: '#FCFCFD', isCore: true },
+            100: { value: '{colors.white}', isCore: true },
         },
         error: {
             0: { value: '{colors.black}', isCore: true },
