f87ce3fb8eaf97339c9d0a59509033b7b29781b2
memoryStack <anujabo186@gmail.com>
2023-12-11 10:08:26 +0530

    Add contract for Empty Rectangle Hint

--- src/apps/arena/utils/smartHints/emptyRectangle/emptyRectangle.ts
+++ src/apps/arena/utils/smartHints/emptyRectangle/emptyRectangle.ts
@@ -1,3 +1,13 @@
+/*
+    what we need to find out in an empty rectangle
+        1. Block House
+        2. Row House in which numbers are present in the empty rectangle's block house
+        3. Col House in which numbers are present in the empty rectangle's block house
+        4. Note that will be highlighted
+        5. House With Conjugate Pairs
+        6. Removable Notes Cells
+*/
+
 export const getEmptyRectangleRawHints = (
     mainNumbers: MainNumbers,
     notes: Notes,
