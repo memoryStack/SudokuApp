028616780c149835df08461b5f35ff3bf20df465
shri-hari08 <pandeyshp08@gmail.com>
2023-12-21 21:49:13 +0530

    code for counting frequency in row and column

--- src/apps/arena/utils/smartHints/emptyRectangle/emptyRectangle.ts
+++ src/apps/arena/utils/smartHints/emptyRectangle/emptyRectangle.ts
@@ -9,12 +9,50 @@
 */
 
 import { HOUSE_TYPE } from '../constants'
+<<<<<<< Updated upstream
+=======
+import { BoardIterators } from '../../classes/boardIterators'
+import { getNoteHostCellsInHouse } from '../../util'
+>>>>>>> Stashed changes
 
+function countRowAndColumnFrequency(objects) {
+    const rowsData = []
+    const columnsData = []
+    for (let i = 0; i < objects.length; i++) {
+        const obj = objects[i]
+        // Check if the row present
+        const rowIndex = rowsData.findIndex((rowObj) => rowObj.row === obj.row)
+        if (rowIndex !== -1) {
+        // If the row exists,count increases
+            rowsData[rowIndex].count++
+        } else {
+        // If the row doesn't exist, push it in rowsData
+            rowsData.push({ row: obj.row, count: 1 })
+        }
+        // Check if the column is present
+        const columnIndex = columnsData.findIndex((colObj) => colObj.column === obj.column)
+        if (columnIndex !== -1) {
+            // If the column exists,count increases
+            columnsData[columnIndex].count++
+        }
+        else {
+        // If the column doesn't exist, push it in columnsData
+            columnsData.push({ column: obj.column, count: 1 })
+        }
+    }
+    // returns array of objects containing
+    return { rowsData, columnsData }
+}
 export const findEmptyRectangleInBlock = (note: Note, blockHouse: House, notes: Notes, possibleNotes: Notes) => {
     // const userFilledCells = getNoteHostCellsInHouse(notes)
     // const possibleHostCells = getNoteHostCellsInHouse(possibleNotes)
     // if (!areSameCellsSets(userFilledCells, possibleHostCells)) return null
-    const a = 10
+    // Define a function called countRowAndColumnOccurrences that takes an array of objects as a parameter
+    const storeinfo = countRowAndColumnFrequency(notes)
+    for(let i = 0; i < storeinfo.length(); i++){
+
+    }
+
     return {
         blockHouse: { type: HOUSE_TYPE.BLOCK, num: 4 },
         rowHouse: { type: HOUSE_TYPE.ROW, num: 3 },
@@ -37,11 +75,17 @@ export const getEmptyRectangleRawHints = (
     notes: Notes,
     possibleNotes: Notes,
 ) => {
+<<<<<<< Updated upstream
     for (let i = 0; i < 9; i++) {
         // forHouseEachCell(house : Block , i) {
         //    }
+=======
+    for (let varNote = 1; varNote <= 9; varNote++) {
+        BoardIterators.forEachHouseNum(houseNum => {
+            let arr = getNoteHostCellsInHouse(varNote, { type: 'block', num: houseNum }, notes)
+        })
+>>>>>>> Stashed changes
     }
-    const a = 10
     return {
         blockHouse: { type: HOUSE_TYPE.BLOCK, num: 4 },
         rowHouse: { type: HOUSE_TYPE.ROW, num: 3 },
