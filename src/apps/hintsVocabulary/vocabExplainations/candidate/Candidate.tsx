3ede0a8abe797fa170676fadc717476bbbee25ce
memoryStack <anujabo186@gmail.com>
2023-12-22 18:21:01 +0530

    anuj commit

--- src/apps/hintsVocabulary/vocabExplainations/candidate/Candidate.tsx
+++ src/apps/hintsVocabulary/vocabExplainations/candidate/Candidate.tsx
@@ -10,6 +10,7 @@ const Candidate = () => {
                 '<p>A Candidate in Sudoku is a digit that could be placed into a <a href="cell">cell</a>, but you\'re not'
                 + ' totally sure it goes in that cell yet. Some people call this "pencil marking", since many solvers'
                 + ' write small numbers or other notes in pencil to denote a possible solution.</p>'
+                + 'new line added by anuj'
             }
         />
 
